5b45ff010e6581927c44270758e74c506495b151
Myles Coleman <mylescoleman05@gmail.com>
2022-10-31 15:18:24 -0700

    works in the console

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const request = require('request-promise');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-const apiKey = 'f118fa4301643891f12b54c71f60bbf4';
-const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+const apiKey = 'AIzaSyA7QQAO1InkKyB35IJ-XAKLoBMesPMdXwQ';
 
 app.use(express.json());
 
@@ -13,17 +11,19 @@ app.get('/', (req, res) => {
     res.send('Welcome to Youtube Scraper API.')
 });
 
-//GET Product Details
+//GET Comment Thread Details
 app.get('/videos/:videoId', async (req, res) => {
     const { videoId } = req.params;
+    const baseUrl = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=replies&textFormat=plainText&order=relevance&videoId=${videoId}&key=${apiKey}`;
 
     try {
         const response = await request(`${baseUrl}&url=https://www.youtube.com/watch?v=${videoId}`);
         res.json(response);
+        console.log(response);
+
     } catch (error) {
         res.json(error);
     }
 })
 
-
-app.listen(PORT, () => {console.log(`Server running on http://localhost:${PORT}`)});
+app.listen(PORT, () => {console.log(`Server up and running at http://localhost:${PORT}`)});
